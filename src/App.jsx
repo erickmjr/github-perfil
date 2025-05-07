f28254b5edb7ba17c3938663d48c16ce69d3ae69
erickmjr <erickmellojr@gmail.com>
2025-05-07 00:15:37 -0300

    refatoracao: substituicao do input para nome por um componente com estilizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,24 @@
 import { useState } from "react";
 
 import Perfil from "./components/Perfil";
-import Formulario from "./components/Formulario";
 import ReposList from "./components/RepoList";
+import InputNome from "./components/InputNome";
 
 function App() {
-  // const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
   const [nomeUsuario, setNomeUsuario] = useState('');
 
   return (
     <>
-      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
-      
-
-      {nomeUsuario.length > 4 && (
+      <InputNome setNomeUsuario={setNomeUsuario} />
+      {nomeUsuario.length >=4 && (
         <>
           <Perfil nomeUsuario={nomeUsuario}/>
           <ReposList nomeUsuario={nomeUsuario}/>
         </>
       )}
-
-      {/* {formularioEstaVisivel && (
-          <Formulario />
-        )
-      } */}
-
-      {/* <button
-        type="button" 
-        onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)}
-      > 
-        Toggle form
-
-      </button> */}
     </>
   )
     
-  
-
 }
 
 export default App
